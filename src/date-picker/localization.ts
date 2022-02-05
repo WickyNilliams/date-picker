e345b295384c3c5f44e11637320df67c1d09ebf6
WickyNilliams <WickyNilliams@MBA>
2022-02-05 21:09:37 +0000

    promote placeholder to property rather than part of localization

--- src/date-picker/localization.ts
+++ src/date-picker/localization.ts
@@ -3,7 +3,6 @@ import { en as calendarEn } from '../calendar/localization.js';
 export const en = {
   ...calendarEn,
   buttonLabel: 'Choose date',
-  placeholder: 'YYYY-MM-DD',
   selectedDateMessage: 'Selected date is',
   closeLabel: 'Close window',
 };
